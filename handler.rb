dcd5e3e40392ac919020e2fb55e6a65f0ddccf8a
Gustavo Ramos Lira <gustavo@grlira.com>
2013-01-02 18:38:01 +0000

    -Added a basic implementation of !say

--- handler.rb
+++ handler.rb
@@ -30,22 +30,32 @@ class Handler
         Kernel.puts line
         message = IrcMessage.new line
 		
-		case message.command
-			when 'PRIVMSG', 'NOTICE'
-				handle_chat message
-			when '433'
-				@kernel.nick_in_use
-			when 'PING'
-				@kernel.write "PONG :#{message.content}"
-		end
+	case message.command
+	    when 'PRIVMSG', 'NOTICE'
+	        handle_chat message
+	    when '433'
+	        @kernel.nick_in_use
+	    when 'PING'
+	        @kernel.write "PONG :#{message.content}"
+	end
     end
 
     def handle_chat(message)
-		case message.content
-			when "hi, #{@kernel.nick}"
-				@kernel.privmsg message.target, "hello there #{message.nick}"
-			when "go away, #{@kernel.nick}"
-            	@kernel.disconnect
+        if message.content.start_with? '!'
+            handle_command(message)
+        end
+        case message.content
+            when "hi, #{@kernel.nick}"
+	        @kernel.privmsg message.target, "hello there #{message.nick}"
+	    when "go away, #{@kernel.nick}"
+                @kernel.disconnect
+        end
+    end
+
+    def handle_command(message)
+        case message.content.split[0]
+            when "!say"
+                @kernel.privmsg message.target, message.content.split[1..-1].join(' ')
         end
     end
 end
