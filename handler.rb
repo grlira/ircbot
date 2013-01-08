1f09c6fbcd5115a4f81bdc24c06a635c3ff842ab
Diogo Pinela <diogoid74@hotmail.com>
2013-01-08 14:33:58 +0000

    Put IRC stuff inside a module

--- handler.rb
+++ handler.rb
@@ -1,113 +1,115 @@
 require_relative 'channel'
 
-class IrcMessage
-    attr_reader :content, :nick, :command, :target, :serverName
-    def initialize(line)
-        parts = line.chomp.split(/(?:^| ):/, 3)
-        if line.start_with? ':'
-            prefix, @command, @target = parts[1].split ' ', 3
-            if @target.split.length > 1
-                @target = @target.split[-1]
-            end
-            @content = parts[2]
-            if prefix.split('!').length > 1
-                @nick = prefix.split('!')[0]
+module IRC
+    class Message
+        attr_reader :content, :nick, :command, :target, :serverName
+        def initialize(line)
+            parts = line.chomp.split(/(?:^| ):/, 3)
+            if line.start_with? ':'
+                prefix, @command, @target = parts[1].split ' ', 3
+                if @target.split.length > 1
+                    @target = @target.split[-1]
+                end
+                @content = parts[2]
+                if prefix.split('!').length > 1
+                    @nick = prefix.split('!')[0]
+                else
+                    @serverName = prefix
+                end
+                unless @target and @target.start_with? '#'
+                    @target = @nick
+                end
             else
-                @serverName = prefix
+                @command, @target = parts[0].split
+                @content = parts[1]
             end
-            unless @target and @target.start_with? '#'
-                @target = @nick
+        end
+    end
+    
+    class Handler
+
+        def initialize(kernel)
+            @kernel = kernel
+        end
+
+        def handle(line)
+            Kernel.puts line
+            message = Message.new line
+        	case message.command
+        	    when 'PRIVMSG', 'NOTICE'
+        	        handle_chat message
+        	    when '433'
+        	        @kernel.nick_in_use
+        	    when 'PING'
+        	        @kernel.write "PONG :#{message.content}"
+                when 'JOIN'
+                    @kernel.channels[message.target] = Channel.new message.target
+                when '353'
+                    @kernel.channels[message.target].add_users message.content
+        	end
+        end
+
+        def handle_chat(message)
+            if message.content.start_with? '!'
+                handle_command(message)
             end
-        else
-            @command, @target = parts[0].split
-            @content = parts[1]
+            case message.content
+                when "hi, #{@kernel.nick}"
+    	            @kernel.privmsg message.target, "hello there #{message.nick}"
+            end
         end
-    end
-end
-
-class Handler
-
-    def initialize(kernel)
-        @kernel = kernel
-    end
-
-    def handle(line)
-        Kernel.puts line
-        message = IrcMessage.new line
-    	case message.command
-    	    when 'PRIVMSG', 'NOTICE'
-    	        handle_chat message
-    	    when '433'
-    	        @kernel.nick_in_use
-    	    when 'PING'
-    	        @kernel.write "PONG :#{message.content}"
-            when 'JOIN'
-                @kernel.channels[message.target] = Channel.new message.target
-            when '353'
-                @kernel.channels[message.target].add_users message.content
-    	end
-    end
-
-    def handle_chat(message)
-        if message.content.start_with? '!'
-            handle_command(message)
+    
+        def handle_command(message)
+            components = message.content.split
+            # Take out the ! symbol
+            command = components.shift[1..-1]
+            begin
+                send :"handle_#{command}", message, *components unless command == 'command'
+            # Ignore unrecognized handlers or commands with the wrong arguments
+            rescue NoMethodError, ArgumentError => exception
+                puts "#{exception.inspect}\n#{exception.backtrace.join "\n"}"
+            end
         end
-        case message.content
-            when "hi, #{@kernel.nick}"
-	            @kernel.privmsg message.target, "hello there #{message.nick}"
+    
+        def handle_say(message, target, *words)
+            @kernel.privmsg target, words.join(' ')
         end
-    end
     
-    def handle_command(message)
-        components = message.content.split
-        # Take out the ! symbol
-        command = components.shift[1..-1]
-        begin
-            send :"handle_#{command}", message, *components unless command == 'command'
-        # Ignore unrecognized handlers or commands with the wrong arguments
-        rescue NoMethodError, ArgumentError => exception
-            puts "#{exception.inspect}\n#{exception.backtrace.join "\n"}"
+        def handle_users(message, channel_name)
+            return unless channel = @kernel.channels[channel_name]
+            if users = channel.users
+                @kernel.privmsg message.target, "In channel #{channel.name} I see users: #{users.join ' '}"
+            end
         end
-    end
     
-    def handle_say(message, target, *words)
-        @kernel.privmsg target, words.join(' ')
-    end
-    
-    def handle_users(message, channel_name)
-        return unless channel = @kernel.channels[channel_name]
-        if users = channel.users
-            @kernel.privmsg message.target, "In channel #{channel.name} I see users: #{users.join ' '}"
+        def handle_join(message, channel_name)
+            @kernel.join_channel channel_name
         end
-    end
     
-    def handle_join(message, channel_name)
-        @kernel.join_channel channel_name
-    end
-    
-    def handle_part(message, channel_name = nil)
-        if channel_name
-            @kernel.part_channel channel_name
-        elsif message.target.start_with? '#'
-            @kernel.part_channel message.target.strip
+        def handle_part(message, channel_name = nil)
+            if channel_name
+                @kernel.part_channel channel_name
+            elsif message.target.start_with? '#'
+                @kernel.part_channel message.target.strip
+            end
         end
-    end
     
-    def handle_cycle(message)
-        if message.target.start_with? '#'
-            target = message.target.strip
-            @kernel.part_channel target
-            @kernel.join_channel target
+        def handle_cycle(message)
+            if message.target.start_with? '#'
+                target = message.target.strip
+                @kernel.part_channel target
+                @kernel.join_channel target
+            end
         end
-    end
     
-    def handle_reload(message)
-        @kernel.privmsg message.target, "Reloading handler"
-        load 'handler.rb'
-        @kernel.privmsg message.target, "Done"
-    end
+        def handle_reload(message)
+            @kernel.privmsg message.target, "Reloading handler"
+            load 'handler.rb'
+            @kernel.privmsg message.target, "Done"
+        end
     
-    def handle_quit(message)
-        @kernel.disconnect
+        def handle_quit(message)
+            @kernel.disconnect
+        end
     end
 end
--- ircbot.rb
+++ ircbot.rb
@@ -17,63 +17,63 @@ class TCPSocket
     end
 end
 
-class IrcBot
+module IRC
+    class Bot
+        attr_reader :nick, :connected, :channels
 
-    attr_reader :nick, :connected, :channels
+        def nick_in_use
+            puts "Error: nick #{@nick} is in use on server"
+            disconnect
+        end
 
-    def nick_in_use
-        puts "Error: nick #{@nick} is in use on server"
-        disconnect
-    end
+        def initialize(address, port, nick)
+            @address = address
+            @port = port
+            @nick = nick
+            @connected = false
+            @handler = Handler.new self
+            @channels = {}
+        end
 
-    def initialize(address, port, nick)
-        @address = address
-        @port = port
-        @nick = nick
-        @connected = false
-        @handler = Handler.new self
-        @channels = {}
-    end
+        def join_channel(channel)
+            write "JOIN #{channel}"
+        end
 
-    def join_channel(channel)
-        write "JOIN #{channel}"
-    end
+        def part_channel(channel)
+            if @channels[channel]
+                write "PART #{channel}"
+                @channels[channel] = nil
+            end
+        end
 
-    def part_channel(channel)
-        if @channels[channel]
-            write "PART #{channel}"
-            @channels[channel] = nil
+        def connect
+            @socket = TCPSocket.new @address, @port
+            @socket.gets_nb do |line|
+                @handler.handle line
+            end
+            @socket.puts 'PASS password'
+            @socket.puts "NICK #{@nick}"
+            @socket.puts 'USER gustavo hostname servername :Gustavo Lira'
+            @connected = true
+        end
+
+        def privmsg(target, content)
+            message = "PRIVMSG #{target} :#{content}"
+            write message
+        end
+
+        def write(message)
+            Kernel.puts "Writing #{message}"
+            @socket.puts message
+        end
+
+        def disconnect
+            @socket.puts "QUIT bye"
+            @socket.close
+            @connected = false
+            exit
         end
     end
-
-    def connect
-        @socket = TCPSocket.new @address, @port
-        @socket.gets_nb do |line|
-            @handler.handle line
-        end
-        @socket.puts 'PASS password'
-        @socket.puts "NICK #{@nick}"
-        @socket.puts 'USER gustavo hostname servername :Gustavo Lira'
-        @connected = true
-    end
-
-    def privmsg(target, content)
-        message = "PRIVMSG #{target} :#{content}"
-        write message
-    end
-
-    def write(message)
-        Kernel.puts "Writing #{message}"
-        @socket.puts message
-    end
-
-    def disconnect
-        @socket.puts "QUIT bye"
-        @socket.close
-        @connected = false
-        exit
-    end
-    
 end
 
 if ARGV.length != 3
@@ -81,7 +81,7 @@ if ARGV.length != 3
     exit 1
 end
 
-bot = IrcBot.new *ARGV[0..2]
+bot = IRC::Bot.new *ARGV[0..2]
 bot.connect
 bot.join_channel "#mieicstudents"
 
