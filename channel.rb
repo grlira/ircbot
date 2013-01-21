97bb571ee4c49129d98bc97c048c776951a0b8ae
Diogo Pinela <diogoid74@hotmail.com>
2013-01-21 17:58:24 +0000

    Put Channel inside the IRC module

--- channel.rb
+++ channel.rb
@@ -1,23 +1,25 @@
-class Channel
-    attr_accessor :users, :name, :topic
+module IRC
+    class Channel
+        attr_accessor :users, :name, :topic
 
-    def initialize(name)
-        @users = []
-        @name = name
-        @topic = ''
-    end
+        def initialize(name)
+            @users = []
+            @name = name
+            @topic = ''
+        end
 
-    def add_user(user)
-        @users.push user
-    end
+        def add_user(user)
+            @users.push user
+        end
 
-    def remove_user(user)
-        @users.delete user
-    end
+        def remove_user(user)
+            @users.delete user
+        end
 
-    def add_users(users)
-       users.split.each do |user|
-          add_user user
-       end 
+        def add_users(users)
+           users.split.each do |user|
+              add_user user
+           end 
+        end
     end
 end
